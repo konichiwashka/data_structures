311230c5bdca9907086a06229515bf7f4e958960
vladenesuk3 <lololitaalololita@gmail.com>
2021-09-17 20:19:08 +0300

    general tree

--- bucket_sort.py
+++ bucket_sort.py
@@ -1,10 +1,9 @@
-import random
 from bubble_sort import bubble_sort
-import time
 
 
 def bucket_sort(my_list, n):
     bucket = [[] for x in range(n)]
+    my_list_sorted = []
     var = max(my_list)/n
     z = 0
     if z == n:
@@ -12,14 +11,15 @@ def bucket_sort(my_list, n):
     else:
         for z in range(n):
             bucket[z] = [my_list[x] for x in range(len(my_list)) if (z+1)*var >= my_list[x] >= z*var+1]
-    # print(bucket)
     for x in range(n):
         bubble_sort(bucket[x-1])
-    # print(bucket)
+    for j in range(n):
+        my_list_sorted += bucket[j-1]
 
 
 def bucket_sort_alternative_version(my_list, n):
     bucket = [[] for x in range(n)]
+    my_list_sorted = []
     var = max(my_list)/n
     z = 0
     if z == n:
@@ -29,19 +29,7 @@ def bucket_sort_alternative_version(my_list, n):
             for x in range(len(my_list)):
                 if (z+1)*var >= my_list[x] >= z*var+1:
                     bucket[z].append(my_list[x])
-    # print(bucket)
     for x in range(n):
         bubble_sort(bucket[x-1])
-    # print(bucket)
-
-
-my_list = [random.randint(100, 1000) for x in range(1, 1000)]
-res = time.time()
-for x in range(100):
-    bucket_sort(my_list, 5)
-print((time.time()-res)/100)
-res1 = time.time()
-for x in range(100):
-    bucket_sort_alternative_version(my_list, 5)
-print((time.time()-res1)/100)
-
+    for j in range(n):
+        my_list_sorted += bucket[j-1]
--- general_tree.py
+++ general_tree.py
@@ -2,9 +2,25 @@ class General_tree(object):
     def __init__(self, data):
         self.data = data
         self.children = []
+        self.parent = None
 
-    def insert(self, data):
-        if self.data in None:
-            data = General_tree(data)
-        else:
-            self.children.append(data)
+    def add_child(self, data):
+        data.parent = self
+        self.children.append(data)
+
+    def insert_child(self, node, subtree: list):
+        for x in subtree:
+            node.add_child(General_tree(x))
+
+
+def create_node(name_of_subtree, list_of_components):
+    name_of_subtree = General_tree(f'{name_of_subtree}')
+    root.add_child(name_of_subtree)
+    root.insert_child(name_of_subtree, list_of_components)
+
+
+list_laptop = ['Mac', 'Win', 'Lin']
+list_cell_phone = ['Iphone', 'Samsung', 'Xiaomi', 'Huawei']
+root = General_tree('Electronics')
+create_node('laptop', list_laptop)
+create_node('cell phone', list_cell_phone)
